51731810a4fb8c9c02f4911103fc0f8a7120d027
Logan <108277501+OreWa-Luffy@users.noreply.github.com>
2024-06-28 23:35:25 +0100

    Added in f1 driver data

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
 import pandas as pd
-import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
-
+df = pd.read_csv(filepath_or_buffer="archive/drivers_updated.csv")
+print(df.head())
